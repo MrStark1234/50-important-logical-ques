0aa86a9cdfee4d0369d476cfd42276aadbf9f513
Suraj Singhal <117528760+MrStark1234@users.noreply.github.com>
2023-11-26 20:43:24 +0530

    EmptyArray Solution

---
+++ Series of 50 Most important logical Questions/emptyArray.js
@@ -0,0 +1,8 @@
+//How to Empty an Array in JavaScript
+//Do not reset it to an New Array and do not run any loop to pop the elements
+//And also don't write any function.
+
+let arr = [1, 2, 3, 4];
+console.log(arr);
+arr.length = 0;
+console.log(arr);
