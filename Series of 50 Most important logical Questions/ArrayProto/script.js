a0f35543576a268ffecccbba46fd54d198d6f9b5
Suraj Singhal <117528760+MrStark1234@users.noreply.github.com>
2024-01-03 15:26:13 +0530

    ArrayProto1

--- Series of 50 Most important logical Questions/ArrayProto/script.js
+++ Series of 50 Most important logical Questions/ArrayProto/script.js
@@ -1,8 +1,9 @@
 // Run a For-In loop on an array and console log it's original properties / indexes. ?
 // without showing its additional properties
 
-let arr = [1, 2, 3, 4];
-for (let i in arr) {
-  if (arr.hasOwnPeoperty(i)) {
-  }
-}
+// let arr = [1, 2, 3, 4];
+// for (let i in arr) {
+//   if (arr.hasOwnPeoperty(i)) {
+//     console.log(i);
+//   }
+// }
